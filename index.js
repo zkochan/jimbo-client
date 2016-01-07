fc597a7a40921eab71adbae67f71b0c4b99fc04f
Zoltan Kochan <zoltan.kochan@gmail.com>
2016-01-08 01:45:16 +0200

    Project created

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const uva = require('uva-amqp')
+
+module.exports = function(plugin, opts, next) {
+  plugin.decorate('server', 'client', function(opts) {
+    let client = new uva.Client({
+      channel: opts.channel,
+      url: opts.url,
+    })
+
+    client.register(opts.methods)
+
+    this.clients[opts.name] = client.methods
+  })
+
+  next()
+}
+
+module.exports.attributes = {
+  pkg: require('./package.json'),
+}
